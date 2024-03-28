48ab8b736aa90a0a5ee7623f7fc7fea6fac485c5
Pankaj yadav <ronnyried@gmail.com>
2024-03-28 23:34:07 +0530

    boxes

--- src/app/boxLighting/page.tsx
+++ src/app/boxLighting/page.tsx
@@ -1,14 +1,57 @@
+'use client'
 import withCenteredLayout from '@/HOCs/useCenterHOC'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
 
 const BoxLight = () => {
-   let boxes = [{id:1}, {id: 2},{id: 3},{id: 4},{id: 5},{id: 6}]
+   let color = 'bg-red-400'
+   let b:any = [{id:1}, {id: 2},{id: 3},{id: 4},{id: 5},{id: 6}]
+    
+   const [boxes, setBoxes] = useState(b)
+   const sequence:number[] = useRef([])
+    
+   let timer:any = useRef()
+
+   const updateBoxes = (index, isfill=true)=>{
+    boxes[index] = {id: boxes[index].id, color: isfill ?color :''}
+    let newValue = [...boxes]
+    setBoxes(newValue)
+   }
+
+   const startOff = ()=>{
+    timer.current = setInterval(()=>{
+        let id = sequence.current.shift()
+        let itemIndex = boxes.findIndex((item)=> id=== item.id)
+        updateBoxes(itemIndex, false)
+        if(sequence.current.length ==0){
+          clearInterval(timer.current)
+        }
+    },1000)
+
+    
+   }
+
+
+   const handleClick = (index)=>{
+    console.log("ind++", sequence.length, boxes.length)
+        sequence.current.push(boxes[index].id)
+         updateBoxes(index)
+        
+        if(sequence.current.length === boxes.length){
+          console.log("ininsdf++ last")
+          startOff()
+        }
+   }
+
+
+  
+
 
   return (
     <div className={'space-x-4 flex flex-row'}>
      {boxes.map((item, index)=>{
         return (
-            <div key={index.toString()} className={`h-24 w-24 bg-red-400 border-cyan-50 border-4`}> </div>
+            <div onClick={()=>handleClick(index)} key={index.toString()} className={`h-24 w-24 ${item?.color} border-cyan-50 border-4`}> </div>
         )
      })}
     </div>
